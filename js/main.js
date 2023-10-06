fceb192bc56c5f25b40debac2a33bd458f8db9af
Brandon 'Okidokie98' de Baat <baat0004@hz.nl>
2023-10-06 10:05:40 +0200

    Cleanup Updates
    Code cleanup and small additions to some blog posts

--- js/main.js
+++ js/main.js
@@ -1,64 +1,15 @@
-// // When the user scrolls the page, execute myFunction
-// window.onscroll = function() {myFunction()};
-
-// // Get the navbar
-// var navbar = document.getElementById("navbar");
-
-// // Get the offset position of the navbar
-// var sticky = navbar.offsetTop;
-
-// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-// function myFunction() {
-//   if (window.scrollY >= sticky) {
-//     navbar.classList.add("sticky")
-//   } else {
-//     navbar.classList.remove("sticky");
-//   }
-// }
-
-
-/* Set the width of the side navigation to 250px */
+/* Set the width of the side navigation to 500px */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "500px";
-  }
-  
-  /* Set the width of the side navigation to 0 */
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
-
-
-  let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+  document.getElementById("mySidenav").style.width = "500px";
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
 }
 
 function move() {
-  var elem = document.getElementById("myBar");   
+  var elem = document.getElementById("myBar");
   var width = 0;
   var id = setInterval(frame, 165);
   function frame() {
@@ -67,8 +18,8 @@ function move() {
       document.getElementById("myP").className = "w3-text-black w3-animate-opacity";
       document.getElementById("myP").innerHTML = "24 Years Old (1998-12-25)";
     } else {
-      width++; 
-      elem.style.width = width + '%'; 
+      width++;
+      elem.style.width = width + '%';
       var num = width * 1 / 1;
       num = num.toFixed(0)
       document.getElementById("demo").innerHTML = num;
